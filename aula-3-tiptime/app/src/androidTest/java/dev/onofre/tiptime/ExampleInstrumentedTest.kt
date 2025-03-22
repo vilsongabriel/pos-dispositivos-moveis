979e1d40d6919ebfd4d8bd7dc67e86a59635ba7a
Vilson Onofre <vilson@onofre.dev>
2025-03-22 15:06:49 -0300

    teste instrumentação tiptime

--- aula-3-tiptime/app/src/androidTest/java/dev/onofre/tiptime/ExampleInstrumentedTest.kt
+++ aula-3-tiptime/app/src/androidTest/java/dev/onofre/tiptime/ExampleInstrumentedTest.kt
@@ -1,24 +1,39 @@
 package dev.onofre.tiptime
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.hamcrest.Matchers.containsString
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.text.NumberFormat
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+
     @Test
     fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("dev.onofre.tiptime", appContext.packageName)
+        onView(withId(R.id.cost_of_service))
+            .perform(typeText("50"))
+            .perform(ViewActions.closeSoftKeyboard())
+
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+
+        val expectedTip = NumberFormat.getCurrencyInstance().format(10).toString()
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString(expectedTip))))
     }
 }
